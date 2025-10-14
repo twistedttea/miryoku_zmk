44ec6072585e47d02d0c3a3210e268708a9d1daa
twistedttea <twistedtteavr@gmail.com>
2025-10-14 01:43:35 -0500

    push that shit homes

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,15 +1,14 @@
 // Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/
-#define XXX &
+// https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_LAYER_BASE \
- U_NP, &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp APOS,     U_NP,\
- &kp GRAVE, U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O), &kp SEMI,\
- &kp EQUAL, U_LT(U_BUTTON, Z), &kp X,     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,             &kp DOT,   U_LT(U_BUTTON, SLASH), &kp MINUS\
- U_NP ,U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE), U_LT(U_FUN, DEL),  U_NP,              U_NP, U_NP
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp APOS,           \
+U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_LT(U_BUTTON, Z), &kp X,     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,             &kp DOT,   U_LT(U_BUTTON, SLASH), \
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_TAP \
-&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp APOS,   U_NP,  \
-&kp GRAVE,  &kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,   &kp SEMI,          \
-    &kp EQUAL,  &kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH, &kp MINUS, \
-U_NP,     U_NP,              U_NP,              &kp ESC,           &kp BSPC,         &kp TAB,           &kp RET,           &kp SPACE,          &kp DEL,           U_NP,              U_NP, U_NP
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp APOS,           \
+&kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
+&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &kp ESC,           &kp BSPC,         &kp TAB,           &kp RET,           &kp SPACE,          &kp DEL,           U_NP,              U_NP
