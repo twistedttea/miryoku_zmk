731ff8d7c9199cdd01eddc6d11d173a8f0f328b3
twistedttea <twistedtteavr@gmail.com>
2025-10-14 00:36:12 -0500

    Missed typing layer.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,4 +11,4 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC),U_LT(
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp APOS,           \
 &kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
 &kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &kp DEL,           &kp BSPC,         &kp TAB,           &kp RET,           &kp SPACE,          &kp ESC,           U_NP,              U_NP
+U_NP,              U_NP,              &kp ESC,           &kp BSPC,         &kp TAB,           &kp RET,           &kp SPACE,          &kp DEL,           U_NP,              U_NP
