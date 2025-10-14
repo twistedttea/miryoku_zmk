35d6b4b31aa6e95eb13f59a29d32c617337f1d4f
twistedttea <twistedtteavr@gmail.com>
2025-10-13 23:30:46 -0500

    Change thumb clusters

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,14 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SEMI,           \
+U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_LT(U_BUTTON, Z), &kp X,     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,             &kp DOT,   U_LT(U_BUTTON, SLASH), \
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, DEL),U_LT(U_MOUSE, BSPC),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE), U_LT(U_FUN, TAB),  U_NP,              U_NP
+
+#define MIRYOKU_LAYER_TAP \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SEMI,           \
+&kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
+&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &kp ESC,           &kp DEL,         &kp BSPC,           &kp RET,           &kp SPACE,          &kp TAB,           U_NP,              U_NP
